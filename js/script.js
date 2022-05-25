96f683b374b2fe011ad439960be63299deee1460
Egor Dubovik <egorka54896@gmail.com>
2022-05-25 19:48:53 +0300

    feat: created function 'sum'

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 let a = 5;
+
 function mult() {
 	let c = a * 20;
+	return c;
+}
+
+function sum() {
+	let c = a + 20;
 	return c;
 }
 
 
 
 
-
 (function callAllFuction() {
 	console.log(mult());
+	console.log(sum());
 })();
